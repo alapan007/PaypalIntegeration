f865b04c692d600abeaabdf68709b953b685482c
alapan007 <alapan_sur@hotmail.com>
2017-06-23 14:17:43 +0530

    Paypal Sample

---
+++ PaypalIntegeration/Models/Paypal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaypalIntegeration.Models
+{
+    public class Paypal
+    {
+        public string ClientToken { get; set; }
+    }
+}
---
+++ PaypalIntegeration/Models/TransactionResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaypalIntegeration.Models
+{
+    public class TransactionResult
+    {
+        public string TransactionID { get; set; }
+        public string Message { get; set; }
+        public string PaymentID { get; set; }
+    }
+}
